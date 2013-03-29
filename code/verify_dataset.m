fea6d29b41f21fd645a090cb98aa7f7bc18e54c4
renaudb <to.rhino@gmail.com>
2013-03-29 14:26:57 -0400

    fix file indentation

--- code/verify_dataset.m
+++ code/verify_dataset.m
@@ -1,4 +1,3 @@
-
 % Verify integrity of the dataset.
 %
 % Author: Sebastian Nowozin <Sebastian.Nowozin@microsoft.com>
@@ -10,21 +9,20 @@ total_frames=0;
 total_gestures=0;
 Ysum=[];
 for di=1:numel(df)
-	[X,Y,tagset]=load_file(strtok(df(di).name,'.'));
-	assert(size(X,1)==size(Y,1));
-	disp(['   ', num2str(di), '/', num2str(numel(df)), ...
-		': "', df(di).name, '": ', num2str(size(X,1)), ' frames, ', ...
+  [X,Y,tagset]=load_file(strtok(df(di).name,'.'));
+  assert(size(X,1)==size(Y,1));
+  disp(['   ', num2str(di), '/', num2str(numel(df)), ...
+	': "', df(di).name, '": ', num2str(size(X,1)), ' frames, ', ...
 		num2str(sum(Y(:))), ' gestures']);
 
-	total_frames=total_frames + size(X,1);
-	total_gestures=total_gestures + sum(Y(:));
+  total_frames=total_frames + size(X,1);
+  total_gestures=total_gestures + sum(Y(:));
 
-	if isempty(Ysum)
-		Ysum=sum(Y);
-	else
-		Ysum=Ysum+sum(Y);
-	end
+  if isempty(Ysum)
+    Ysum=sum(Y);
+  else
+    Ysum=Ysum+sum(Y);
+  end
 end
 disp([num2str(numel(df)), ' sequences, ', num2str(total_frames), ...
-	' frames, ', num2str(total_gestures), ' gestures.']);
-
+      ' frames, ', num2str(total_gestures), ' gestures.']);
