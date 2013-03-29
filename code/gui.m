b60acfed525ba5f919840b976e991e2263cf890a
renaudb <to.rhino@gmail.com>
2013-03-29 18:09:48 -0400

    add gui code

---
+++ code/gui.m
@@ -0,0 +1,139 @@
+function varargout = gui(varargin)
+% GUI MATLAB code for gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui
+
+% Last Modified by GUIDE v2.5 29-Mar-2013 16:30:12
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before gui is made visible.
+function gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui (see VARARGIN)
+
+% Choose default command line output for gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Setup the camera.
+tpos=[0, 0, 2.75];
+cpos=tpos + [3, 2, -5];
+set(handles.viewer,'CameraPosition',cpos);
+set(handles.viewer,'CameraTarget',tpos);
+set(handles.viewer,'CameraViewAngle',12);
+set(handles.viewer,'CameraUpVector',[0 1 0]);
+set(handles.viewer,'Projection','perspective');
+
+% Setup the axes.
+set(handles.viewer,'XLim', [-0.5, 0.5]);
+set(handles.viewer,'YLim', [-1.0, 1.0]);
+set(handles.viewer,'ZLim', [ 2.5, 3.0]);
+axis(handles.viewer, 'equal');
+
+% UIWAIT makes gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in openbutton.
+function openbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to openbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Select a file.
+filename = uigetfile('*.csv','Select clip file');
+if ~filename
+   return
+end
+[p,name,e] = fileparts(filename);
+
+% Set the clip name.
+set(handles.clipname, 'String', name);
+
+% List of tags.
+tagset = { 'G1  lift outstretched arms', 'G2  Duck', ...
+	'G3  Push right', 'G4  Goggles', 'G5  Wind it up', ...
+	'G6  Shoot', 'G7  Bow', 'G8  Throw', 'G9  Had enough', ...
+	'G10 Change weapon', 'G11 Beat both', 'G12 Kick' };
+
+% Load the data from the file.
+[X, Y] = load_file(name, tagset);
+
+% Store the result.
+handles.X = X;
+handles.Y = Y;
+guidata(hObject, handles);
+
+% --- Executes on button press in playbutton.
+function playbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to playbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get the data.
+X = handles.X;
+Y = handles.Y;
+
+% Get the data size.
+T=size(X,1);
+
+% Animate sequence
+for ti=1:T
+  skel_vis(X,ti,handles.viewer);
+  drawnow;
+  pause(1/30);
+  cla;
+end
---
+++ code/skel_vis.m
@@ -0,0 +1,47 @@
+function skel_vis(X,tidx,h);
+%SKEL_VIS -- Visualize a skeleton in 3D coordinates.
+%
+% Input
+%   X: (T,4*NUI_SKELETON_POSITION_COUNT) matrix from load_file.
+%   tidx: time index >=1, <=T.
+%   h: (optional) axes handle to draw in.
+%
+% Author: Sebastian Nowozin <Sebastian.Nowozin@microsoft.com>
+
+assert(tidx >= 1);
+assert(tidx <= size(X,1));
+xyz_ti=X(tidx,:);
+
+skel_model
+skel=reshape(xyz_ti, 4, NUI_SKELETON_POSITION_COUNT)';
+if nargin < 3
+  h=axes;
+end
+
+axes(h);
+plot3(skel(:,1), skel(:,2), skel(:,3), 'bo');
+axis(h, 'equal');
+
+for ci=1:size(nui_skeleton_conn,1)
+  hold on;
+  line([xyz_ti(4*nui_skeleton_conn(ci,1)+1) ; ...
+	xyz_ti(4*nui_skeleton_conn(ci,2)+1)], ...
+	[xyz_ti(4*nui_skeleton_conn(ci,1)+2) ; ...
+	xyz_ti(4*nui_skeleton_conn(ci,2)+2)], ...
+	[xyz_ti(4*nui_skeleton_conn(ci,1)+3) ; ...
+	xyz_ti(4*nui_skeleton_conn(ci,2)+3)]);
+end
+
+tpos=[0, 0, 2.75];
+cpos=tpos + [3, 2, -5];
+set(h,'CameraPosition',cpos);
+set(h,'CameraTarget',tpos);
+set(h,'CameraViewAngle',12);
+set(h,'CameraUpVector',[0 1 0]);
+set(h,'Projection','perspective');
+
+set(h,'XLim', [-0.5, 0.5]);
+set(h,'YLim', [-1.0, 1.0]);
+set(h,'ZLim', [ 2.5, 3.0]);
+
+title(sprintf('Frame %d', tidx));
