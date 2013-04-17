f2241ef3a2dc88517572054e348076f5e42cd63d
renaudb <to.rhino@gmail.com>
2013-04-17 17:27:35 -0400

    cleanup code

--- src/driver.m
+++ src/driver.m
@@ -1,55 +1,12 @@
-[X, Y, relX, z] = load_data('../data/', 1.0, [1:12]);
-train_X=X(1:uint32(size(X,1)*0.8),:);
-train_Y=Y(1:uint32(size(Y,1)*0.8),:);
-val_Y=Y(uint32(size(Y,1)*0.8)+1:end,:);
-val_X=X(uint32(size(X,1)*0.8)+1:end,:);
-train_relX=relX(1:uint32(size(relX,1)*0.8),:);
-val_relX=relX(uint32(size(relX,1)*0.8)+1:end,:);
-
-net=train_net(train_relX, train_Y)
-test_net(net, val_relX, val_X, val_Y)
-
-%train_dX=dX(1:uint32(size(dX,1)*0.8),:);
-%train_dY=dY(1:uint32(size(dY,1)*0.8),:);
-
-%val_dY=dY(uint32(size(dY,1)*0.8)+1:end,:);
-%val_dX=dX(uint32(size(dX,1)*0.8)+1:end,:);
-
-
-% For sliding window. Note that this also skips frames. Sliding window size
-% and skip sizes can be modified in load_data_sliding.
+% Load the data and separate it in training and testing dataset.
 [slided_relX,weighted_Y] = load_data_sliding('../data/', 1.0, [1:12], 5, 5);
 train_relX=slided_relX(1:uint32(size(slided_relX,1)*0.8),:);
 val_relX=slided_relX(uint32(size(slided_relX,1)*0.8)+1:end,:);
 train_Y=weighted_Y(1:uint32(size(weighted_Y,1)*0.8),:);
 val_Y=weighted_Y(uint32(size(weighted_Y,1)*0.8)+1:end,:);
 
-deep_net=train_net(train_relX, train_Y, [100])
-test_net_sliding(deep_net, val_relX, val_Y)
+% Train a network.
+net=train_net(train_relX, train_Y, [25])
 
-
-
-% Recurrent net stuff, doesn't work yet
-[sslided_relX,sweighted_Y] = load_data_sliding('../data/', 1.0, [1:12], 1, 1);
-strain_relX=sslided_relX(1:uint32(size(sslided_relX,1)*0.8),:);
-sval_relX=sslided_relX(uint32(size(sslided_relX,1)*0.8)+1:end,:);
-strain_Y=sweighted_Y(1:uint32(size(sweighted_Y,1)*0.8),:);
-sval_Y=sweighted_Y(uint32(size(sweighted_Y,1)*0.8)+1:end,:);
-
-
-lrn_net = newlrn(strain_relX',strain_Y', 50);
-lrn_net.trainFcn = 'trainbr';
-lrn_net.trainParam.show = 5;
-lrn_net.trainParam.epochs = 1;
-lrn_net = train(lrn_net,strain_relX',strain_Y');
-
-recnet = layrecnet(1:2,[50],'trainscg')
-recnet = train(recnet, strain_relX', strain_Y')
-
-recnet = configure(recnet, strain_relX', strain_Y')
-
-[Xs,Xi,Ai,Ts] = preparets(net,strain_relX,strain_Y)
-
-test_net_sliding(snet, sval_relX, sval_Y)
-
-
+% Test the network.
+test_net_sliding(net, val_relX, val_Y)
